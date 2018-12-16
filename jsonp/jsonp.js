66d345d8de0ae4addeb4fa41ffa097aa41138fcc
zwhsunshine <zhengwenhui9309@163.com>
2018-12-16 17:26:03 +0800

    修改-jsonp.js-1216

--- jsonp/jsonp.js
+++ jsonp/jsonp.js
@@ -5,39 +5,53 @@ function jsonp(obj){
 		let opt = {
 			url:'',
 			data:{},
-			callback:'callback'
+			callback:('jQuery'+Date.now()+'_'+Math.random()).replace('.','')
 		}
 
 		//有配置走配置，无配置走默认
 		Object.assign(opt,obj);
 
 		//回调函数名字设置为随机，以防与全局（window）下其他的函数名重复
-		let fn = 'jQuery' + '_' + Date.now() + '_' + Math.random();
+		let fn = opt.callback;
 		
-		fn = fn.replace('.',''); //去小数点
-
 		//请求超时
-		opt[fn] = setTimeout(function(){
-			reject('请求超时');
-		},20000)
+		if(window.onOff === undefined) window.onOff = false;
+		if(window.onOff === false){
+			window.onOff = true;
+			opt[fn] = setTimeout(function(){
+				reject('请求超时');
+			},10000)
+		}
+		
 
 		//请求数据
 		window[fn] = function(data){
 			clearTimeout(opt[fn]);
+			delete window[fn];
+			window.onOff = false;
 			resolve(data);
 		}
 
-		//存放查询信息
-		let arr = [];
 
 		//在opt上加一个属性为 opt.callback的值，并且将函数名字赋值给它，使得data中有callback的值
 		opt.data[opt.callback] = fn;
 
-		//遍历opt.data的时候可以遍历到查询信息和回调函数，得到类似这种效果(aa=123&bb=ff&callback=fn);
-		for( let attr of Object.keys(opt.data) ){
-			arr.push( attr + '=' + opt.data[attr] );
-		}
-		opt.data = arr.join('&');
+		//以下有两种方法：用于把对象转成 a=1&b=f&c=g 这种格式的
+
+			//第一种方法：
+			/*
+				//存放查询信息
+				let arr = [];
+
+				//遍历opt.data的时候可以遍历到查询信息和回调函数，得到类似这种效果(aa=123&bb=ff&callback=fn);
+				for( let attr of Object.keys(opt.data) ){
+					arr.push( attr + '=' + opt.data[attr] );
+				}
+				opt.data = arr.join('&');
+			*/
+
+		//第二种方法：
+		opt.data = new URLSearchParams(opt.data).toString();
 
 		//创建script
 		let scri = document.createElement('script');
