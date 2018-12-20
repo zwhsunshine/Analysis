295d7ed4e32b4a4a7dcc468dd39ea979ad47636c
zwhsunshine <zhengwenhui9309@163.com>
2018-12-21 03:40:16 +0800

    修改-1221

--- login-node/login.js
+++ login-node/login.js
@@ -1,8 +1,10 @@
 
+//加载（引入）模块
 let http = require('http'),
 	fs = require('fs'),
 	qs = require('querystring');
 
+//数据
 let sql = [
 	{
 		user:'aa',
@@ -18,47 +20,97 @@ let sql = [
 	}
 ];
 
+//后台返回到前端的数据
 let info = {
 	code:0,
 	msg:'success'
 }
 
+//创建一个http服务器
 http.createServer((req,res)=>{
 
-	let url = req.url,
-		obj = qs.parse(uel.split('?')[0]);
+	//请求的地址
+	let url = req.url;
 
 	//接口
 	if(url.includes('?')){
 
-		let d = sql.find(e=>e.user == obj.user);
+		//获取由?切割 得到的 接口信息
+		let arr = url.split('?');
+		let obj = qs.parse(url.split('?')[1]);
 
-		//用户名存在
-		if(d.user){
+		//登录
+		if(arr[0] === '/login'){
 
+			//用户名存在
+			if(obj.user && obj.pw && /^[A-Za-z]\w{1,7}/.test(obj.user) ){
 
-		}else{  //用户名不存在
+				let u = sql.find(e=>e.user == obj.user);
 
-			info.code = 1;
-			info.msg = '请输入用户名'
+				//有效的用户名
+				if(u){
+					
+					//密码正确
+					if(u.pw == obj.pw){
 
+						info.code = 0;
+						info.msg = '登录成功';
+
+					}else{  //密码不正确
+
+						info.code = 3;
+						info.msg = '密码不正确';
+
+					}
+
+				}else{  //此用户名不存在
+
+					info.code = 2;
+					info.msg = '此用户名不存在';
+
+				}
+
+				res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+
+			}else{  //用户名或密码未输入
+
+				info.code = 1;
+				info.msg = '请输入用户名或密码';
+
+				res.writeHead(400,{'Content-Type':'text/html; charset=utf-8'});
+
+			}
+			res.write(JSON.stringify(info));
+			res.end();
 		}
 
 	}else{  //静态文件
 
-		fs.readFile('page/index.html',function(err,data){
+		if( url === '/' ) url = '/index.html';   //page/index.html
+
+		//读取页面 page目录下的文件
+		fs.readFile('./page'+url,function(err,data){
+
+			//找不到文件，跳转至404页面
 			if(err){
+
 				let data = fs.readFileSync('page/error.html');
 				res.write(data);
-			}else{
+
+			}else{  //找到文件读取文件
+
 				res.write(data);
+
 			}
+
+			//结束响应
 			res.end();
+
 		})
 
 	}
 
-})
+}).listen(80);  //端口号必写
 
 
 
